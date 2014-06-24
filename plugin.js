b950e9434c5e08c8cc5deccfe87f1b2cfc90bb42
ekoneko <ekoneko9@gmail.com>
2014-06-24 21:07:32 +0800

    update yen list

--- plugin.js
+++ plugin.js
@@ -2,27 +2,17 @@
 /*global $:true */
 // require yen/style.css
 
-(function () {
+(function() {
     'use strict';
 
     var YenTexts = [
-        ' \\(￣ˇ￣)> ', ' \\(>。<)> ', ' ∑(っ °Д °;)っ ',
-        ' ˋ( ° ▽、°）', ' <(￣3￣)> ', ' ╰(￣▽￣)╮ ',
-        ' ╮(╯▽╰)╭ ', ' (づ￣▽￣)づ ', ' \\>。</ ', ' (||| ￣,_ゝ￣) ',
-        ' V(￣ ▽ ￣)V ', ' ( ˉ▽ˉ；) ', ' =___,=y~~ ',
-        ' \\("▔□▔)/ ', ' (～￣▽￣)～ ', '（＾Ｏ＾）',
-        ' 罒▽罒 ', ' ╭(╯^╰)╮ ', ' \\(≥▽≤)/ ',
-        ' ~(～ o ～)~zZ~~ ', ' /(ㄒoㄒ)/~ ', ' (¬_¬) ',
-        ' \\(ㄒ_ㄒ)> ', ' (*´∇‘*) ', ' \\(＾Ｏ＾)> ',
-        ' \\(°▽、°)> ', ' \\(￣▽￣)> ', ' \\(≥▽≤)> ',
-        ' \\(╯^╰)> ', ' \\(ˉ▽ˉ；)> ', ' (#‘′)凸 ',
-        ' ╮(罒w罒)╭ ', ' =。= ', ' (O_O)？'
+        ' |∀ﾟ ', ' (´ﾟДﾟ`) ', ' (;´Д`) ', ' (｀･ω･) ', ' (=ﾟωﾟ)= ', ' | ω・´) ', ' |-` ) ', ' |д` ) ', ' |ー` ) ', ' |∀` ) ', ' (つд⊂) ', ' (ﾟДﾟ≡ﾟДﾟ) ', ' (＾o＾)ﾉ ', ' (|||ﾟДﾟ) ', ' ( ﾟ∀ﾟ) ', ' ( ´∀`) ', ' (*´∀`) ', ' (*ﾟ∇ﾟ) ', ' (*ﾟーﾟ) ', ' (　ﾟ 3ﾟ) ', ' ( ´ー`) ', ' ( ・_ゝ・) ', ' ( ´_ゝ`) ', ' (*´д`) ', ' (・ー・) ', ' (・∀・) ', ' (ゝ∀･) ', ' (〃∀〃) ', ' (*ﾟ∀ﾟ*) ', ' ( ﾟ∀。) ', ' ( `д´) ', ' (`ε´ ) ', ' (`ヮ´ ) ', ' σ`∀´) ', '  ﾟ∀ﾟ)σ ', ' ﾟ ∀ﾟ)ノ ', ' (╬ﾟдﾟ) ', ' (|||ﾟдﾟ) ', ' ( ﾟдﾟ) ', ' Σ( ﾟдﾟ) ', ' ( ;ﾟдﾟ) ', ' ( ;´д`) ', ' (　д ) ﾟ ﾟ ', ' ( ☉д⊙) ', ' (((　ﾟдﾟ))) ', ' ( ` ・´) ', ' ( ´д`) ', ' ( -д-) ', ' (&gt;д&lt;) ', ' ･ﾟ( ﾉд`ﾟ) ', ' ( TдT) ', ' (￣∇￣) ', ' (￣3￣) ', ' (￣ｰ￣) ', ' (￣ . ￣) ', ' (￣皿￣) ', ' (￣艸￣) ', ' (￣︿￣) ', ' (￣︶￣) ', ' ヾ(´ωﾟ｀) ', ' (*´ω`*) ', ' (・ω・) ', ' ( ´・ω) ', ' (｀・ω) ', ' (´・ω・`) ', ' (`・ω・´) ', ' ( `_っ´) ', ' ( `ー´) ', ' ( ´_っ`) ', ' ( ´ρ`) ', ' ( ﾟωﾟ) ', ' (oﾟωﾟo) ', ' (　^ω^) ', ' (｡◕∀◕｡) ', ' /( ◕‿‿◕ )\\ ', ' ヾ(´ε`ヾ) ', ' (ノﾟ∀ﾟ)ノ ', ' (σﾟдﾟ)σ ', ' (σﾟ∀ﾟ)σ ', ' |дﾟ ) ', ' ┃電柱┃ ', ' ﾟ(つд`ﾟ) ', ' ﾟÅﾟ )　 ', ' ⊂彡☆))д`) ', ' ⊂彡☆))д´) ', ' ⊂彡☆))∀`) '
     ];
 
-    tinymce.PluginManager.add('yen', function (editor) {
+    tinymce.PluginManager.add('yen', function(editor) {
         var showDialog, createDialog, createItem, choseYen;
 
-        createDialog = function (x, y) {
+        createDialog = function(x, y) {
             var dialog, i;
             dialog = $('<div class="tinymce-yen-box mce-menu"><div>').css({
                 left: x + 'px',
@@ -33,28 +23,28 @@
             for (i in YenTexts) {
                 dialog.append(createItem(YenTexts[i]));
             }
-            setTimeout(function () {
-                $(document).on('click.yentext', function () {
+            setTimeout(function() {
+                $(document).on('click.yentext', function() {
                     $(document).off('click.yentext');
                     dialog.remove();
                 });
             }, 1);
         };
 
-        createItem = function (text) {
+        createItem = function(text) {
             var item;
             item = $('<div class="tinymce-yen-item">' + text + '</div>');
-            item.on('click', function () {
+            item.on('click', function() {
                 choseYen(this.innerText);
             });
             return item;
         };
 
-        choseYen = function (text) {
+        choseYen = function(text) {
             editor.insertContent(text);
         };
 
-        showDialog = function (event) {
+        showDialog = function(event) {
             createDialog(event.pageX, event.pageY);
         };
 
@@ -63,5 +53,5 @@
             tooltip: 'Yen text',
             onclick: showDialog
         });
-   });
+    });
 }());
